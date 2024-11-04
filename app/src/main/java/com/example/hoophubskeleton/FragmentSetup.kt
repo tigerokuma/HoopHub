97c96d094ace3794ac69158c1d44021fd6d026c8
KrisTretiak <ktretiak@sfu.ca>
2024-11-03 21:24:01 -0800

    -fixed git

--- app/src/main/java/com/example/hoophubskeleton/FragmentSetup.kt
+++ app/src/main/java/com/example/hoophubskeleton/FragmentSetup.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-
+//
 class FragmentSetup (
     private val context: Context,
     private val topTabMenu: TabLayout,
