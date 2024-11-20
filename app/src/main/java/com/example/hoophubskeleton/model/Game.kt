a74303c1142d46c98b3e3949dcc0f32aebec507c
Paul Atwal <paulatwal_@hotmail.com>
2024-11-20 14:02:22 -0800

    Added Game, GamesRepository, and GameViewModel

---
+++ app/src/main/java/com/example/hoophubskeleton/model/Game.kt
@@ -0,0 +1,17 @@
+package com.example.hoophubskeleton.model
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
+
+// This represents a game stored in Firebase. These game objects are created when one
+// player sends another player an invite to a game.
+
+class Game(
+    var createdBy: String = "",
+    var sentTo: String = "",
+    var gameDateTime: Timestamp = Timestamp.now(),
+    var inviteAccepted: Boolean= false,
+    var location: GeoPoint = GeoPoint(0.0, 0.0), // latLng?
+    var timestamp: Timestamp = Timestamp.now() // when invite created
+) {
+}
